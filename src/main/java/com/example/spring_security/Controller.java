c22efbe59e1226e1153036f503f6f04981cff93f
sunny raj <sunnyrajuph@gmail.com>
2025-11-30 21:41:08 +0530

    2nd commit

--- src/main/java/com/example/spring_security/Controller.java
+++ src/main/java/com/example/spring_security/Controller.java
@@ -1,5 +1,6 @@
 package com.example.spring_security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,17 @@ public class Controller {
     @GetMapping("/hello")
     public String  printSomething(){
         return "Hello World!...";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String  printUser(){
+        return "Hello User!...";
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/admin")
+    public String  printAdmin(){
+        return "Hello Admin!...";
     }
 }
